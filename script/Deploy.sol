2eb1c87b6684b6d5f28a5689315862c0e210c453
조영국 <ethan@MacBookPro.local>
2025-02-22 19:31:17 +0900

    feat: write deployment code

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+import "../contracts/core/DAOGovernance.sol";
+import "../contracts/core/Treasury.sol";
+import "../contracts/core/DAOToken.sol";
 import "forge-std/Script.sol";
 
 contract Deploy is Script {
@@ -9,7 +12,16 @@ contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
 
-        // ...
+        DAOToken daoToken = new DAOToken();
+        Treasury treasury = new Treasury(address(daoToken));
+        DAOGovernance daoGovernance = new DAOGovernance();
+
+        daoToken.initialize(address(daoGovernance), address(treasury));
+        treasury.setGovernance(address(daoGovernance));
+
+        console.log("DAOGovernance:", address(daoGovernance));
+        console.log("DAOToken:", address(daoToken));
+        console.log("Treasury:", address(treasury));
 
         vm.stopBroadcast();
     }

253bb34f1ea665e6e3ef561753db864380a0d533
조영국 <ethan@MacBookPro.local>
2025-02-22 20:54:54 +0900

    feat: create Proposal struct

--- src/core/DAOGovernance.sol
+++ src/core/DAOGovernance.sol
@@ -1,13 +1,46 @@
-// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.10;
 
 import "../utils/EmergencyStop.sol";
 import "../utils/Multicall.sol";
 
 contract DAOGovernance is EmergencyStop, Multicall {
+    /**
+     * @dev Struct to store information about a proposal
+     * 
+     * This struct is used to manage the details of a proposal in the DAO.
+     * 
+     * @param id Unique identifier for the proposal
+     * @param proposer Address of the user who proposed
+     * @param description Description of the proposal
+     * @param target Address of the contract to execute the proposal
+     * @param data Data to execute (function call data)
+     * @param startTime Start time of the proposal
+     * @param endTime End time of the proposal
+     * @param votesFor Number of votes in favor
+     * @param votesAgainst Number of votes against
+     * @param executed Indicates whether the proposal has been executed
+     * @param isActive Indicates whether the proposal is active
+     */
+    struct Proposal {
+        uint256 id;
+        address proposer;
+        string description;
+        address target;
+        bytes data;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 votesFor;
+        uint256 votesAgainst;
+        bool executed;
+        bool isActive;
+    }
+
     mapping(uint256 => uint256) public proposals;
 
     function propose() external {}
     function vote(uint256 proposalId) external {}
     function execute(uint256 proposalId) external {}
+    function tallyVotes(uint256 proposalId) external {}
+    function getProposal(uint256 proposalId) external view returns (Proposal memory) {}
 }

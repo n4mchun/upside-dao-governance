7008a34e07fc18fc3e26923b252863d05fec14bc
조영국 <ethan@MacBookPro.local>
2025-02-22 20:23:18 +0900

    feat: add proxy deployment

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "../contracts/core/DAOGovernance.sol";
-import "../contracts/core/Treasury.sol";
-import "../contracts/core/DAOToken.sol";
+import "../src/core/DAOGovernance.sol";
+import "../src/core/Treasury.sol";
+import "../src/core/DAOToken.sol";
+import "../src/proxy/DAOProxy.sol";
 import "forge-std/Script.sol";
 
 contract Deploy is Script {
@@ -15,6 +16,10 @@ contract Deploy is Script {
         DAOToken daoToken = new DAOToken();
         Treasury treasury = new Treasury(address(daoToken));
         DAOGovernance daoGovernance = new DAOGovernance();
+        
+        // data is empty yet
+        DAOProxy proxy = new DAOProxy(address(daoGovernance), bytes(""));
+        
 
         daoToken.initialize(address(daoGovernance), address(treasury));
         treasury.setGovernance(address(daoGovernance));
@@ -22,6 +27,7 @@ contract Deploy is Script {
         console.log("DAOGovernance:", address(daoGovernance));
         console.log("DAOToken:", address(daoToken));
         console.log("Treasury:", address(treasury));
+        console.log("DAOProxy:", address(proxy));
 
         vm.stopBroadcast();
     }
